9ad65a17b6bd714e599e6f1b1aa9b637c75eeba6
joekariuki3 <joekariuki3@gmail.com>
2024-05-23 14:06:02 +0300

    Refactor  the look of verifying email look. add documentation for the functions

--- frontend/accotrac/src/pages/VerifyEmail.jsx
+++ frontend/accotrac/src/pages/VerifyEmail.jsx
@@ -3,7 +3,13 @@ import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import baseUrl from '../utils/settings';
 
+
 axios.defaults.withCredentials = true;
+/**
+ * VerifyEmail component is responsible for verifying the email address of a user.
+ *
+ * @return {JSX.Element} The JSX element representing the VerifyEmail component.
+ */
 const VerifyEmail = () => {
   const navigate = useNavigate();
   const [message, setMessage] = useState('');
@@ -13,6 +19,16 @@ const VerifyEmail = () => {
   // verify email component
   // using useEffect to verify email on the backend
   useEffect(() => {
+    /**
+     * Verifies the user's email by making an HTTP GET request to the backend API.
+     * If the request is successful, sets the message state with the response data message and
+     * navigates to the '/login' route after a delay of 1 second.
+     * If the request fails, throws an error with the response data message.
+     * If there is an error other than a failed request, sets the message state with an error message
+     * and logs the error to the console.
+     *
+     * @return {Promise<void>} - A promise that resolves when the verification is complete.
+     */
     const verifyEmail = async () => {
       try {
         const response = await axios.get(`${baseUrl}/user/verifyemail/${token}`);
@@ -38,20 +54,18 @@ const VerifyEmail = () => {
   }, [navigate, token, setMessage]);
 
   return (
-    <div className="container d-flex justify-content-center align-items-center vh-100">
-      <div className="row">
-        <div className="col-lg-8">
-          <div className="card mt-5">
-            <div className="card-body">
-              <h2 className="text-center mb-4">Email Verification</h2>
-              <div className="text-center mb-4">
-                {message && <h2 style={{ color: "blue", fontSize: '30px', fontWeight: '700' }}>{message}</h2>}
-              </div>
-            </div>
+   <main className="d-flex flex-column min-vh-100 justify-content-center align-items-center bg-light">
+      <div className="mx-auto w-full max-w-md p-4 space-y-6 rounded-lg bg-white shadow-lg">
+        <div className="text-center space-y-4">
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
           </div>
+          <h1 className="text-lg font-bold">Verifying Email</h1>
+          <p className="text-muted">Please wait while we verify your email address.</p>
+          <p>{message}</p>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
