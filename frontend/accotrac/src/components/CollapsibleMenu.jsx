9fc79d5b8b64d22292daaa87f79019951d3d9249
joekariuki3 <joekariuki3@gmail.com>
2024-05-24 08:42:13 +0300

    Refactor code for CollapsibleMenu to look better

--- frontend/accotrac/src/components/CollapsibleMenu.jsx
+++ frontend/accotrac/src/components/CollapsibleMenu.jsx
@@ -7,16 +7,15 @@ import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import baseUrl from '../utils/settings';
+import './collapsible-menu.css';
 
 axios.defaults.withCredentials = true;
 
 function CollapsibleMenu() {
-  // navbar to be displayed when user is on a smaller screen
   const navigate = useNavigate();
   const { setCompany, setUser, user, company } = useContext(UserContext);
   const [isOpen, setIsOpen] = useState(false);
 
-  // fuction to logout a user
   const logoutUser = async () => {
     try {
       const response = await axios.post(`${baseUrl}/logout`);
@@ -38,48 +37,49 @@ function CollapsibleMenu() {
     }
   };
 
-  // to open or close Navbar
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <Navbar bg="primary" expand="lg" variant="dark">
-      <Navbar.Brand>
-        {company && (
-          <Link to='/dashboard' style={{ color: 'white' }}>{company.name}</Link>
-        )}
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleMenu} />
-      <Collapse in={isOpen} id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          {user ? (
-            <>
-              <Link to="/home" className="nav-link" style={{ color: 'white' }}>Home</Link>
-              <Link to="/about" className="nav-link" style={{ color: 'white' }}>About Us</Link>
-              <Link to="/contact" className="nav-link" style={{ color: 'white' }}>Contact Us</Link>
-              <Link to="/dashboard" className="nav-link" style={{ color: 'white' }}>Dashboard</Link>
-              <Link to="/addaccount" className="nav-link" style={{ color: 'white' }}>Add Account</Link>
-              <Link to="/chartsofaccount" className="nav-link" style={{ color: 'white' }}>Charts of Account</Link>
-              <Link to="/generaljournal" className="nav-link" style={{ color: 'white' }}>Add Journal</Link>
-              <Link to="/journals" className="nav-link" style={{ color: 'white' }}>View Journals</Link>
-              <Link to="/trialbalance" className="nav-link" style={{ color: 'white' }}>Trial Balance</Link>
-              <Link to="/profitloss" className="nav-link" style={{ color: 'white' }}>Profit/Loss</Link>
-              <Link to="/balancesheet" className="nav-link" style={{ color: 'white' }}>Balance Sheet</Link>
-              <Link to="/myprofile" className="nav-link" style={{ color: 'white' }}>My Profile</Link>
-              <Link to="/companyprofile" className="nav-link" style={{ color: 'white' }}>Organization Profile</Link>
-              <Link onClick={logoutUser} className="nav-link" style={{ color: 'white' }}>Sign Out</Link>
-            </>
-          ) : (
-            <>
-              <Link to="/home" className="nav-link" style={{ color: 'white' }}>Home</Link>
-              <Link to="/about" className="nav-link" style={{ color: 'white' }}>About Us</Link>
-              <Link to="/contact" className="nav-link" style={{ color: 'white' }}>Contact Us</Link>
-              <Link to="/login" className="nav-link" style={{ color: 'white' }}>Sign In</Link>
-              <Link to="/signup" className="nav-link" style={{ color: 'white' }}>Get Started</Link>
-            </>
-          )}
-        </Nav>
-      </Collapse>
-    </Navbar>
+    <div className="collapsible-menu">
+      <button className="btn btn-outline-primary btn-open-menu" onClick={toggleMenu}> ||| </button>
+      <Navbar
+        bg="primary"
+        expand="lg"
+        variant="dark"
+        className={`collapsed-menu ${isOpen ? 'active' : ''}`}
+      >
+        <Collapse in={isOpen} id="basic-navbar-nav">
+          <Nav className="" style={{ color: 'red' }}>
+            {user ? (
+              <>
+              <Link to="/home" className="nav-link">Home</Link>
+              <Link to="/about" className="nav-link">About Us</Link>
+              <Link to="/contact" className="nav-link">Contact Us</Link>
+              <Link to="/dashboard" className="nav-link">Dashboard</Link>
+              <Link to="/addaccount" className="nav-link">Add Account</Link>
+              <Link to="/chartsofaccount" className="nav-link">Charts of Account</Link>
+              <Link to="/generaljournal" className="nav-link">Add Journal</Link>
+              <Link to="/journals" className="nav-link">View Journals</Link>
+              <Link to="/trialbalance" className="nav-link">Trial Balance</Link>
+              <Link to="/profitloss" className="nav-link">Profit/Loss</Link>
+              <Link to="/balancesheet" className="nav-link">Balance Sheet</Link>
+              <Link to="/myprofile" className="nav-link">My Profile</Link>
+              <Link to="/companyprofile" className="nav-link">Organization Profile</Link>
+              <Link onClick={logoutUser} className="nav-link">Sign Out</Link>
+              </>
+            ) : (
+              <>
+              <Link to="/home" className="nav-link">Home</Link>
+              <Link to="/about" className="nav-link">About Us</Link>
+              <Link to="/contact" className="nav-link">Contact Us</Link>
+              <Link to="/login" className="nav-link">Sign In</Link>
+              <Link to="/signup" className="nav-link">Get Started</Link>
+              </>
+            )}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 }
 
