86fdebdef66110c2fd12ab6ecbca997d2cf7ead6
joelmuhoho <joekariuki3@gmail.com>
2026-07-06 13:35:10 +0300

    refactor: update accotracStatus to use environment variables for dynamic API domain configuration

--- frontend/accotrac/src/utils/settings.js
+++ frontend/accotrac/src/utils/settings.js
@@ -1,9 +1,15 @@
-const accotracStatus = "production";
+const accotracStatus = process.env.APPLICATION_STATUS;
+const accotracApiProductionDomain = process.env.API_DOMAIN;
+const accotracApiTestingDomain = process.env.TESTING_API_DOMAIN;
 let baseUrl = "";
 if (accotracStatus === "production") {
   baseUrl = "api";
+} else if (accotracStatus === "production-cpanel") {
+  baseUrl = accotracApiProductionDomain;
+} else if (accotracStatus === "testing-cpanel") {
+  baseUrl = accotracApiTestingDomain;
 } else if (accotracStatus === "development") {
-  baseUrl = "http://localhost:5000";
+  baseUrl = "";
 } else {
   baseUrl = "api";
 }
